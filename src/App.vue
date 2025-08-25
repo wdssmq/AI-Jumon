6bfc2990ac697abdabc76797d737ab167e041bd0
wdssmq <wdssmq@qq.com>
2025-08-25 20:28:27 +0800

    up. 移除示例样式

--- src/App.vue
+++ src/App.vue
@@ -8,34 +8,14 @@ import HelloWorld from './components/HelloWorld.vue'
   <p>
     <strong>Current route path:</strong> {{ $route.fullPath }}
   </p>
-  <!-- <nav>
+  <nav class="flex gap-4 mb-4">
     <RouterLink to="/">Go to Home</RouterLink>
     <RouterLink to="/about">Go to About</RouterLink>
-  </nav> -->
+  </nav>
   <main>
     <RouterView />
   </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-nav {
-  display: flex;
-  justify-content: space-around;
-}
-nav a {
-  text-decoration: none;
-  color: #42b983;
-}
 </style>
