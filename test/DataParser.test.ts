cb690c36cbe823ff0966fb874827ecbb04ae9171
wdssmq <wdssmq@qq.com>
2025-09-09 12:05:52 +0800

    test. 核心库测试代码

--- test/DataParser.test.ts
+++ test/DataParser.test.ts
@@ -8,7 +8,7 @@ const doM = (i: number, fn: Function) => {
 };
 
 describe('IndexParser', () => {
-  const jsonData = JSON.stringify([
+  const jsonData = JSON.stringify(
     {
       items: [
         { name: 'base', content: '女性, 二次元少女' },
@@ -16,13 +16,15 @@ describe('IndexParser', () => {
         { name: '动物', content: '桌子上有{{$动物类型}}, 沙发上有{{$动物类型}}' },
         { name: '窗户', content: '{{rnd(1,-1)}}' },
         { name: '窗外景色', content: '{{if($窗户 ? 窗外有蓝天白云 : _null)}}' },
+        { name: '窗台', content: '{{if($窗户 ? 窗台上有{{$动物类型}} : _null)}}' },
       ],
       prompts: [
         { name: 'demo', content: '{{base}}, 坐在椅子上, {{动物}}' },
         { name: 'demo2', content: '{{base}}, 坐在椅子上, {{窗外景色}},,' },
+        { name: 'demo3', content: '{{base}}, 桌子上有{{$动物类型}}, {{窗台}}, {{窗外景色}}' },
       ],
     },
-  ]);
+  );
 
   it('should generate the correct prompt', () => {
     const parser = new IndexParser(jsonData);
@@ -32,21 +34,31 @@ describe('IndexParser', () => {
 
   it('should generate the correct prompt', () => {
     const parser = new IndexParser(jsonData);
-    doM(10, () => {
+    doM(20, () => {
       const result = parser.generatePrompt('demo2');
       expect(result).toBeOneOf(['女性, 二次元少女, 坐在椅子上', '女性, 二次元少女, 坐在椅子上, 窗外有蓝天白云' ]);
     });
   });
 
+  it('随机及条件判断测试', () => {
+    const parser = new IndexParser(jsonData);
+    doM(15, () => {
+      const result = parser.generatePrompt('demo3');
+      console.log(result);
+    });
+  });
+
+
+
   it('should list all prompts', () => {
     const parser = new IndexParser(jsonData);
     const prompts = parser.listPrompts();
-    expect(prompts).toEqual(expect.arrayContaining(['demo', 'demo2']));
+    expect(prompts).toEqual(expect.arrayContaining(['demo', 'demo2', 'demo3']));
   });
 
   it('should list all items', () => {
     const parser = new IndexParser(jsonData);
     const items = parser.listItems();
-    expect(items).toEqual(expect.arrayContaining(['base', '动物类型', '动物']));
+    expect(items).toEqual(expect.arrayContaining(['base', '动物类型', '动物', '窗户', '窗外景色', '窗台']));
   });
 });
